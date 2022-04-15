1e23c83bf08d7fb0e672d6988ce73744dbe24de2
Savkiv <ivan.savkiv.igorovych>
2022-04-15 15:27:33 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from "./logo.svg";
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 
 import { AboutPage, HomePage, PostsPage, NotFoundPage } from "./pages";
 
 function App() {
   return (
-    <header>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/posts">Posts</a>
-      <a href="/not_found">Not Found</a>
-    </header>
+    <>
+      <header>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/posts">Posts</a>
+        <a href="/not_found">Not Found</a>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/posts" element={<PostsPage />}></Route>
+        <Route path="/not_found" element={<NotFoundPage />}></Route>
+      </Routes>
+    </>
   );
 }
 

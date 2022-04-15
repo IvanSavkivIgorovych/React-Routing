bdfbd148d1beb51b2be43d0886e57246f2377c1c
Savkiv <ivan.savkiv.igorovych>
2022-04-15 22:54:49 +0300

    Add parse posts function in PostPage.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,34 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import React from "react";
 
 export const Layout = (props) => {
   return (
     <>
       <header>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/not_found">Not Found</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "blue" : "white")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "blue" : "white")}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/posts"
+          className={({ isActive }) => (isActive ? "blue" : "white")}
+        >
+          Posts
+        </NavLink>
+        <NavLink
+          to="/not_found"
+          className={({ isActive }) => (isActive ? "blue" : "white")}
+        >
+          Not Found
+        </NavLink>
       </header>
       <Outlet />
     </>
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const PostsPage = () => {
-  return <div>PostsPage</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, []);
+  if (!posts.length) return null;
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
 };
